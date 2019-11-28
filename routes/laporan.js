b182b2e7151c9ae7f9f3fb83df2d6abb9406bab3
alansarluv <alan.sarluf@gmail.com>
2019-11-29 02:09:19 +0700

    auth for dmr laporan site

--- routes/laporan.js
+++ routes/laporan.js
@@ -2,8 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const laporanController = require('../controllers/laporan');
+const isAuth = require('../middleware/is-auth');
+const isDMR = require('../middleware/is-dmr');
 const router = express.Router();
 
-router.get('/laporan', laporanController.getIndex);
+router.get('/laporan', isAuth, isDMR, laporanController.getIndex);
 
 module.exports = router;
